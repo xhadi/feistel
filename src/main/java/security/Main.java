c3cc00995afcc23a577d11573bc88ac26f8fb06a
Hadi Almubarak <hadi.almubarak3@gmail.com>
2025-11-23 20:24:02 +0300

    Implement MainFrame class with GUI components and add application logo

--- src/main/java/security/Main.java
+++ src/main/java/security/Main.java
@@ -2,6 +2,7 @@ package security;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
 }
